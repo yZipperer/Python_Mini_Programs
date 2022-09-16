19739d39471754c3fb99ca16e169fd10afdd6f69
Yukon Z <yukonz@Yukons-MacBook-Air.local>
2022-09-15 19:47:53 -0600

    Added name generator program

---
+++ RandomNameGenerator.py
@@ -0,0 +1,6 @@
+import random
+
+first_names = ["John", "Sam", "Alice", "Jarold", "Hannah", "Jennifer", "George", "Daniel", "Kylie"]
+last_names = ["Johnson", "Smith", "Davis", "Wilson", "Williams", "Brown", "Garcia", "Miller"]
+
+print(random.choice(first_names) + " " + random.choice(last_names))
