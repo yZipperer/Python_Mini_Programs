da0a9b62688e543dcf53bbbdbbcd65eddd8a7a58
Yukon Z <yukonz@yukons-air.uguest.utah.edu>
2022-09-15 20:06:31 -0600

    Added New Program

---
+++ RandomStreetNameGenerator.py
@@ -0,0 +1,6 @@
+import random
+
+street_name_first = ["Sonora", "Sonoma", "SunnyBrook", "Thompson", "Thorton", "Venice", "Windsor", "Alto", "Abbey"]
+street_name_last = ["Street", "Lane", "Road", "Highway"]
+
+print(random.choice(street_name_first) + " " + random.choice(street_name_last))
